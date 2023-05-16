679c903e6884a174ded021fb7bbaae75d3a4c525
baezdev <baezdev@gmail.com>
2023-05-16 01:53:58 -0600

    Add svg icons

---
+++ src/components/Icons.jsx
@@ -0,0 +1,31 @@
+export function AddIcon () {
+  return (
+    <svg width='20px' height='20px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 12H9M12 12H20M12 4V20' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function BitacoraIcon () {
+  return (
+    <svg width='20px' height='20px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M15 12L10.8 16.2L9.00003 14.4M9 4.5V6H15V4.5M9 4.5V3H15V4.5M9 4.5H5V14M15 4.5H19V8M19 13V21H5V18' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function CloseIcon () {
+  return (
+    <svg width='30px' height='30px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M19 5L5 19M5 5L9.5 9.5M12 12L19 19' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function MenuIcon () {
+  return (
+    <svg width='30px' height='30px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 17H8M12 17H20M4 12H20M4 7H12M16 7H20' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
