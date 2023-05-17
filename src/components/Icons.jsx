914c85e2b3430ab16f30f4b29471429ec4a06fe4
baezdev <baezdev@gmail.com>
2023-05-16 23:22:28 -0600

    Add new icon

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -29,3 +29,11 @@ export function MenuIcon () {
     </svg>
   )
 }
+
+export function SaveIcon () {
+  return (
+    <svg width='20px' height='20px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 13V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V16M4 8V6C4 4.89543 4.89543 4 6 4H14.1716C14.702 4 15.2107 4.21071 15.5858 4.58579L19.4142 8.41421C19.7893 8.78929 20 9.29799 20 9.82843V12M15 20V15H9V20' stroke='#fff' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
