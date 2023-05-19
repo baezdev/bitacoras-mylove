26effb199f09dcaa453cd5c6c819a71560796a49
baezdev <baezdev@gmail.com>
2023-05-19 13:47:47 -0600

    Add new register to bd

---
+++ src/service/logbooks.js
@@ -0,0 +1,19 @@
+import { db, doc, setDoc, getDoc } from '../config/firebase.config'
+
+export async function addNewRegister (newRegister) {
+  const optionsDate = { month: 'long', year: 'numeric' }
+  const instanceDate = new Date()
+  const getDate = instanceDate.toLocaleDateString(undefined, optionsDate).replace(' de', ',')
+
+  try {
+    const docRef = doc(db, 'logbooks', `${getDate}`)
+    const sales = (await getDoc(docRef)).data().sales || []
+    await setDoc(docRef, {
+      sales: [...sales, newRegister]
+    })
+
+    return 'Registro Guardado'
+  } catch (error) {
+    return `Error: ${error}`
+  }
+}
