6e118727096eb316f93a4fcc6ed747bdefbc248c
baezdev <baezdev@gmail.com>
2023-05-19 13:47:15 -0600

    Config firebase

---
+++ src/config/firebase.config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
+/* import { getAnalytics } from 'firebase/analytics' */
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBF6JuXGCMHKHvrnJL2yjhNI7pa0NlRQO4',
+  authDomain: 'mylove-bitacoras.firebaseapp.com',
+  projectId: 'mylove-bitacoras',
+  storageBucket: 'mylove-bitacoras.appspot.com',
+  messagingSenderId: '227992067733',
+  appId: '1:227992067733:web:99e5ac7b86273b67d0059c',
+  measurementId: 'G-C3LNBF7L8Q'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+/* const analytics = getAnalytics(app) */
+
+export {
+  db, doc, setDoc, getDoc
+}
