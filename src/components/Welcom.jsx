3a97a3236ea11228b79ead9a4a98bda0879eb4e1
baezdev <baezdev@gmail.com>
2023-05-27 23:09:56 -0600

    add responsive design

--- src/components/Welcom.jsx
+++ src/components/Welcom.jsx
@@ -1,8 +1,8 @@
 export function Welcome () {
   return (
-    <section className='py-10 basis-1/2'>
-      <h2 className='text-4xl font-semibold'>Bienvenida a</h2>
-      <h3 className='text-6xl font-semibold justify-start flex gap-2'><span className='text-pink-400 font-black'>MyLove</span> Bitacoras</h3>
+    <section className='py-2 md:py-10 basis-1/2'>
+      <h2 className='text-2xl md:text-4xl font-semibold'>Bienvenida a</h2>
+      <h3 className='text-4xl md:text-6xl font-semibold justify-start flex gap-2'><span className='text-pink-400 font-black'>MyLove</span> Bitacoras</h3>
     </section>
   )
 }
