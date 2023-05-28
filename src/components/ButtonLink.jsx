fa55e859bd95df700f1f9a19c15c3a5e25d2ec35
baezdev <baezdev@gmail.com>
2023-05-27 23:08:45 -0600

    change file name

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,11 +1,12 @@
-export function Button ({ children, onClick }) {
+export function ButtonLink ({ children, href, download }) {
   return (
-    <button
+    <a
+      href={href}
       className='py-3 px-20 w-fit font-semibold text-lg capitalize bg-pink-400 text-white rounded flex items-center gap-1 transition-all hover:scale-105 hover:bg-pink-500'
-      type='button'
-      onClick={onClick}
+      role='button'
+      download={download}
     >
       {children}
-    </button>
+    </a>
   )
 }
