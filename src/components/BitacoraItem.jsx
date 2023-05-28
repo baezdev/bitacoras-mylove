736c313318371ee712e0a1d253861130a720bac1
baezdev <baezdev@gmail.com>
2023-05-27 23:12:42 -0600

    add create file

--- src/components/BitacoraItem.jsx
+++ src/components/BitacoraItem.jsx
@@ -1,29 +1,21 @@
-import { Button } from './Button'
-import { getRegisterByQuincena } from '../service/bitacoras'
-import { createExcel } from '../service/excel'
+import { ButtonLink } from './ButtonLink'
 
 export function BitacoraItem ({ bitacora }) {
-  const handleDownloadFile = async (bitacora, quincena) => {
-    const result = await getRegisterByQuincena(bitacora, quincena)
-    console.log(result)
-    createExcel()
-  }
-
   return (
-    <li className='p-8 flex flex-col gap-8 border-b border-[#cac7e1]'>
-      <h3 className='capitalize text-4xl font-medium text-center'>{bitacora}</h3>
-      <div className='flex gap-10 justify-center'>
-        <div className='flex flex-col items-center gap-3'>
-          <p className='text-xl'>Primer Quincena</p>
-          <Button onClick={() => handleDownloadFile(bitacora, 1)}>
-            Descargar Bitacora
-          </Button>
+    <li className='pb-5 md:p-8 flex flex-col gap-4 md:gap-8 border-b border-[#cac7e1]'>
+      <h3 className='capitalize text-2xl md:text-4xl font-medium text-center'>{bitacora}</h3>
+      <div className='flex flex-col md:flex-row gap-5 md:gap-10 justify-center'>
+        <div className='flex flex-col items-center gap-1 md:gap-3'>
+          <p className='text-lg'>Primer Quincena</p>
+          <ButtonLink href={`/download?quincena=1&date=${bitacora}`}>
+            Generar Excel
+          </ButtonLink>
         </div>
-        <div className='flex flex-col items-center gap-3'>
-          <p className='text-xl'>Segunda Quincena</p>
-          <Button onClick={() => handleDownloadFile(bitacora, 2)}>
-            Descargar Bitacora
-          </Button>
+        <div className='flex flex-col items-center gap-1 md:gap-3'>
+          <p className='text-lg'>Segunda Quincena</p>
+          <ButtonLink href={`/download?quincena=2&date=${bitacora}`}>
+            Generar Excel
+          </ButtonLink>
         </div>
       </div>
     </li>
